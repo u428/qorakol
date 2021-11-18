0ae3f2be595030d4873a59008600d973bfb8593f
Muahammad Amin <43309816+u428@users.noreply.github.com>
2021-11-19 00:25:34 +0500

    vvvvvvvvvvvvvvvvvvv

--- src/main/java/com/qorakol/ilm/ziyo/repository/AuthRepository.java
+++ src/main/java/com/qorakol/ilm/ziyo/repository/AuthRepository.java
@@ -7,8 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface AuthRepository extends JpaRepository<AuthEntity, Long> {
 
-//    @Query(value = "select cast(case when count(*) > 0 then 1 else 0 end as bit) from sys_auth_entity s where s.login = :login", nativeQuery = true)
-//    boolean findByLoginExists(String login);
 
     AuthEntity findByLogin(String login);
 
