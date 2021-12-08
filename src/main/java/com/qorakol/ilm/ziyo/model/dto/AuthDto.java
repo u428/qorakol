15f70dfeb7662f524252877d3c11d75558889779
Muahammad Amin <43309816+u428@users.noreply.github.com>
2021-12-08 23:04:28 +0500

    fix

--- src/main/java/com/qorakol/ilm/ziyo/model/dto/AuthDto.java
+++ src/main/java/com/qorakol/ilm/ziyo/model/dto/AuthDto.java
@@ -1,13 +1,6 @@
 package com.qorakol.ilm.ziyo.model.dto;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import lombok.Data;
 
-import javax.validation.constraints.NotEmpty;
-
-@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
-@Data
 public class AuthDto {
 
     public String login;
