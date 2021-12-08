ad7ae9d2d477caadef2d8c46fa8187bb3548bc02
Muahammad Amin <43309816+u428@users.noreply.github.com>
2021-12-08 23:06:37 +0500

    fix

--- src/main/java/com/qorakol/ilm/ziyo/model/dto/AuthDto.java
+++ src/main/java/com/qorakol/ilm/ziyo/model/dto/AuthDto.java
@@ -1,24 +1,11 @@
 package com.qorakol.ilm.ziyo.model.dto;
 
+import lombok.Data;
 
+@Data
 public class AuthDto {
 
     public String login;
     public String password;
 
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
